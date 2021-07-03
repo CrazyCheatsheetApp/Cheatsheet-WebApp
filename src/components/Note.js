bc0c37c754edcf10524b92868933f7550372bdbe
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2021-07-03 18:02:57 +1200

    ui

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,7 +6,7 @@ const Note = ({ id, text, date, handleDeleteNote }) => {
         <div className="note grow-note">
             <div className="note-header">
                 <span>{text}</span>
-                <MdClose onClick={() => handleDeleteNote(id)} className="grow-delete delete-icon" size="1.3em" />
+                <MdClose onClick={() => handleDeleteNote(id)} className="delete-icon" size="1.3em" />
             </div>
             <div className="note-footer">
                 <small>
