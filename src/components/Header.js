37ff31802b68f5c24c1704591c259e2866aec5fe
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2021-07-04 10:17:35 +1200

    change font on h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     const { signOut } = useContext(AuthContext);
     return (
         <div className="header">           
-            <h1>Cheat Sheet</h1>
+            <h1><span className="cheat">Cheat</span> Sheet</h1>
             <button onClick={signOut} className="signOut grow">Signout</button>
         </div>
     );
